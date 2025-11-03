599dd89f7e754520f24a8a762c08e4a856784645
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-11-04 00:40:40 +0700

    Add routes [Login, Signup, Home]

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import { Routes } from "react-router"
+import { Route, Routes } from "react-router"
+import Layout from "./layouts/layout"
+import Home from "./pages/home"
+import Login from "./pages/login"
+import Signup from "./pages/signup"
 
 function App() {
   return (
     <Routes>
-      
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />}></Route>
+        <Route path="login" element={<Login />}></Route>
+        <Route path="signup" element={<Signup />}></Route>
+      </Route>
     </Routes>
   )
 }
