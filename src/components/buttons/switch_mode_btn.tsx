349b6f12533128114fe1769c01e8866f41be44d5
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-11-04 01:08:05 +0700

    Fix error, handle savedMode value when localStorage theme is null

--- src/components/buttons/switch_mode_btn.tsx
+++ src/components/buttons/switch_mode_btn.tsx
@@ -1,24 +1,28 @@
 import { useState } from "react"
 
 function SwitchModeBtn() {
+    let html = document.documentElement;
+
     const [mode, setMode] = useState<string>(() => {
-        const savedMode = localStorage.getItem('theme');
-        return savedMode ? savedMode : 'light';
+        let savedMode = localStorage.getItem('theme');
+        
+        if (!savedMode) savedMode = 'light';
+        html.setAttribute('data-theme', savedMode);
+
+        return savedMode;
     });
 
     const toggleMode = () => {
         const newMode = (mode == 'light') ? 'dark' : 'light';
 
         setMode(newMode);
-        
-        let html = document.documentElement;
         html.setAttribute('data-theme', newMode);
 
         localStorage.setItem('theme', newMode);
     }
 
     return (
-        <button className={`bg-primary text-white`} title="Switch Mode" onClick={toggleMode}>
+        <button className={`w-9 h-9 rounded-md flex items-center justify-center bg-primary text-white`} title="Switch Mode" onClick={toggleMode}>
             <i className={`${(mode == 'light' ? 'ri-moon-line' : 'ri-sun-line')} text-2xl `} />
         </button>
     )
