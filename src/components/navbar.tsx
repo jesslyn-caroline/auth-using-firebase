6691922582c670eeaef75786717e5ffaaa93a1b0
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-11-04 23:24:52 +0700

    Add height and bg-color

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,7 +2,7 @@ import SwitchModeBtn from "./buttons/switch_mode_btn"
 
 function Navbar() {
     return (
-        <div className={`w-full flex flex-row items-center justify-end`}>
+        <div className={`w-full min-h-16 h-16 bg-bg flex flex-row items-center justify-end px-6 lg:px-10 fixed`}>
             <SwitchModeBtn />
         </div>
     )
