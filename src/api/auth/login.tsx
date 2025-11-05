ce5629b2bf50794afd776468131334018cef2ffa
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-11-05 10:51:01 +0700

    Change Login Status

--- src/api/auth/login.tsx
+++ src/api/auth/login.tsx
@@ -3,6 +3,8 @@ import ToastSuccess from "../../components/toasts/toast_success";
 import app from "../../firebase/firebase-config";
 import ToastError from "../../components/toasts/toast_error";
 import { useNavigate } from "react-router";
+import { useContext } from "react";
+import { ProfileContext } from "../../providers/profile_provider";
 
 interface Props {
     email: string,
@@ -13,13 +15,16 @@ function login() {
     const auth = getAuth();
     const navigate = useNavigate();
 
+    const { changeLoginStatus } = useContext(ProfileContext);
+
     async function loginWithEmailandPassword({email, password}: Props): Promise<void>{
         app;
         try {
             await signInWithEmailAndPassword(auth, email, password)
             ToastSuccess("🚀 Login Successfully!");
+            changeLoginStatus(email);
             navigate("/");
-    
+
         } catch (error : any) {
             if (error.code === "auth/invalid-credential") ToastError("🚫 Invalid Credential!");
         }
--- src/api/auth/signout.tsx
+++ src/api/auth/signout.tsx
@@ -3,16 +3,21 @@ import ToastSuccess from "../../components/toasts/toast_success";
 import ToastError from "../../components/toasts/toast_error";
 import app from "../../firebase/firebase-config";
 import { useNavigate } from "react-router";
+import { useContext } from "react";
+import { ProfileContext } from "../../providers/profile_provider";
 
 function signout() {
     const auth = getAuth();
     const navigate = useNavigate();
+
+    const { changeLoginStatus } = useContext(ProfileContext);
 
     async function signOutfromAcc():Promise<void> {
         app;
         try {
             await signOut(auth)
             ToastSuccess("🚀 Sign Out Successfully!")
+            changeLoginStatus("");
             navigate("/login");
         } catch (error: any) {
             ToastError("🚫 Sign Out Failed!")
