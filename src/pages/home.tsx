a621bcda23a53774f5023b2a4239e783aa0f45e3
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-11-05 10:51:12 +0700

    Update content

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,19 @@
 import { useContext } from "react"
 import { ProfileContext } from "../providers/profile_provider";
+import BlockBtn from "../components/buttons/block_btn";
+import signout from "../api/auth/signout";
 
 function Home() {
 
     const { email } = useContext(ProfileContext);
+    const { signOutfromAcc } = signout();
 
     return (
-        <div className={`pt-16 `}>
-            <h1 className={`font-bold`}>Hello, {email}</h1>
+        <div className={`pt-16 flex flex-col gap-y-4`}>
+            <h1 className={`font-bold text-xl text-text`}>Hello, {email}</h1>
+            <div className={`w-30`}>
+                <BlockBtn title={"Logout"} action={signOutfromAcc} />
+            </div>
         </div>
     )
 }
