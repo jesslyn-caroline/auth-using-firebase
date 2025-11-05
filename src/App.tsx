4f64919035db774082877aa8ae16fd967d54db30
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-11-05 10:50:36 +0700

    Redirect to login if user doesn't login

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { Route, Routes } from "react-router"
+import { Navigate, Route, Routes } from "react-router"
 import Layout from "./layouts/layout"
 import Home from "./pages/home"
 import Login from "./pages/login"
 import Signup from "./pages/signup"
+import { useContext } from "react"
+import { ProfileContext } from "./providers/profile_provider"
 
 function App() {
+  const { email } = useContext(ProfileContext);
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />}/>
+        <Route index element={(email === "" ? <Navigate to="/login" /> : <Home />)}/>
         <Route path="login" element={<Login />}/>
         <Route path="signup" element={<Signup />}/>
       </Route>
